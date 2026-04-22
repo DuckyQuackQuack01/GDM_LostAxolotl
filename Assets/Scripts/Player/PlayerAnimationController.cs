cc293d66d2418271c7eec0b15a1cc25fc4419dfc
lilamcostco <liam.costa@student.uts.edu.au>
2026-04-22 21:09:37 +1000

    Added slingshot progression animation and updated axolotl sprite

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -10,9 +10,9 @@ public class PlayerAnimationController : MonoBehaviour
     Vector2 currentVelocity;
     float rotationSpeed;
 
-    Vector2 startMousePosition;
+    Vector3 startMousePosition;
 
-    [SerializeField] Animator animator;
+    public Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,7 +24,9 @@ public class PlayerAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AnimatorStateInfo animState = anim.GetCurrentAnimatorStateInfo(0);
         currentVelocity = rb.linearVelocity;
+
         UpdateRotation();
 
         if (Input.GetMouseButtonDown(0))
@@ -33,7 +35,11 @@ public class PlayerAnimationController : MonoBehaviour
         }
         if (Input.GetMouseButton(0) && currentVelocity == Vector2.zero)
         {
-            UpdateSlingshotAnimation();
+            UpdateSlingshot();
+        }
+        else if (currentVelocity == Vector2.zero && !animState.IsName("Idle"))
+        {
+            PlayIdleAnim();
         }
     }
 
@@ -43,19 +49,39 @@ public class PlayerAnimationController : MonoBehaviour
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
-    void UpdateSlingshotAnimation()
+    private void UpdateSlingshot()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 directionAway = startMousePosition - Input.mousePosition;
+        UpdateSlingshotRotation(directionAway);
+        UpdateSlingshotAnimation(directionAway);
+    }
+    private void OnMouseRelease()
+    {
+        Debug.Log("Mouse released");
+    }
 
-        Vector2 directionAway = startMousePosition - mousePosition;
+    private void UpdateSlingshotRotation(Vector2 directionAway)
+    {
 
         float angle = Mathf.Atan2(directionAway.y, directionAway.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
-    private void MouseButtonDown()
+    private void UpdateSlingshotAnimation(Vector2 directionAway)
     {
-        
+        float percentage = directionAway.magnitude / 100;
+        if (percentage > 0.99f)
+        {
+            percentage = 0.99f;
+        }
+        Debug.Log(percentage);
+        anim.Play("ReadyingSlingshot", 0, percentage);
     }
+
+    private void PlayIdleAnim()
+    {
+        anim.Play("Idle");
+    }
+    
 }
