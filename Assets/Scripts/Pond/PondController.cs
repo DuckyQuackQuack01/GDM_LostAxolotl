c7975ce265d91c9420e4c29e4c25f17b4628c079
DuckyQuackQuack01 <sergsfarc@gmail.com>
2026-05-07 11:32:31 +1000

    Fixed gravity transfer between levels & level 1 platform collision

--- Assets/Scripts/Pond/PondController.cs
+++ Assets/Scripts/Pond/PondController.cs
@@ -3,10 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class PondController : MonoBehaviour
 {
+    private Vector2 defaultGravity = new Vector2(0f, -9.8f);
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            Physics2D.gravity = defaultGravity;
+
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentSceneIndex + 1);
         }
